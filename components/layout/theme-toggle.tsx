6f38255d061117c3377e1ff86e6d6a32fe1ab739
usefmahmud <usef.mahmoud@outlook.com>
2025-07-18 15:42:42 +0300

    feat(theme-toggle): enhance theme toggle functionality with improved accessibility and state management

--- components/layout/theme-toggle.tsx
+++ components/layout/theme-toggle.tsx
@@ -1,28 +1,68 @@
 'use client';
 
-import React from 'react';
+import { cn } from '@/lib/utils';
+import React, { useEffect } from 'react';
+
+type THEME = 'light' | 'dark';
 
 const ThemeToggle = () => {
+  const [theme, setTheme] = React.useState<THEME>(
+    (localStorage.getItem('theme') as THEME) || 'dark'
+  );
+
+  const toggleTheme = () => {
+    setTheme((newTheme) => {
+      const nextTheme = newTheme === 'dark' ? 'light' : 'dark';
+      localStorage.setItem('theme', nextTheme);
+      return nextTheme;
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    document.body.classList.toggle('dark', theme === 'dark');
+  }, [theme]);
+
   return (
     <div
-      className='border-foreground flex border-5'
-      role='group'
-      aria-label='Theme selection'
+      className='border-foreground relative flex cursor-pointer overflow-hidden border-5 *:px-4 *:text-[32px] *:font-semibold *:transition-transform *:duration-150 *:ease-in-out'
+      role='button'
+      tabIndex={0}
+      aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} theme`}
+      onClick={() => toggleTheme()}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          e.preventDefault();
+          toggleTheme();
+        }
+      }}
     >
-      <button
-        className={`px-4 text-[32px] font-semibold transition-colors`}
-        aria-label='Switch to light theme'
-        type='button'
-      >
-        LIGHT
-      </button>
-      <button
-        className={`px-4 text-[32px] font-medium transition-colors`}
-        aria-label='Switch to dark theme'
-        type='button'
+      <div
+        className={cn(
+          'bg-foreground absolute inset-y-0 transition-transform duration-150 ease-in-out',
+          'w-1/2',
+          theme === 'light' ? 'translate-x-0' : 'translate-x-full'
+        )}
+      />
+
+      <span
+        className={cn(
+          'relative z-10 flex items-center justify-center transition-colors duration-300',
+          theme === 'light' ? 'text-background' : 'text-foreground'
+        )}
+        aria-hidden={theme !== 'dark'}
       >
         DARK
-      </button>
+      </span>
+      <span
+        className={cn(
+          'relative z-10 flex items-center justify-center transition-colors duration-300',
+          theme === 'dark' ? 'text-background' : 'text-foreground'
+        )}
+        aria-hidden={theme !== 'light'}
+      >
+        LIGHT
+      </span>
     </div>
   );
 };
