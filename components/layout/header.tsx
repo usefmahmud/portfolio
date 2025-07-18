d97a6b5d0655369dc0bdb5ffa0c5bbb7ad52b0e5
usefmahmud <usef.mahmoud@outlook.com>
2025-07-18 13:33:05 +0300

    feat: add Header component with title

---
+++ components/layout/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='flex justify-between py-3'>
+      <div>
+        <h1 className='text-[40px] font-black select-none'>YOUSEF MAHMOUD</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
