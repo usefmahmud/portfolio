2d39c5faf4e732c59f5ab8f51a3357d29e48b429
usefmahmud <usef.mahmoud@outlook.com>
2025-07-18 13:11:29 +0300

    style: update global styles and add custom font support

---
+++ lib/fonts.ts
@@ -0,0 +1,127 @@
+import localFont from "next/font/local";
+
+export const madeTommyThin = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY Thin_PERSONAL USE.otf",
+  variable: "--font-made-tommy-thin",
+  display: "swap",
+});
+
+export const madeTommyThinOutline = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY Thin Outline_PERSONAL USE.otf",
+  variable: "--font-made-tommy-thin-outline",
+  display: "swap",
+});
+
+export const madeTommyLight = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY Light_PERSONAL USE.otf",
+  variable: "--font-made-tommy-light",
+  display: "swap",
+});
+
+export const madeTommyLightOutline = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY Light Outline_PERSONAL USE.otf",
+  variable: "--font-made-tommy-light-outline",
+  display: "swap",
+});
+
+export const madeTommyRegular = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY Regular_PERSONAL USE.otf",
+  variable: "--font-made-tommy-regular",
+  display: "swap",
+});
+
+export const madeTommyRegularOutline = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY Regular Outline_PERSONAL USE.otf",
+  variable: "--font-made-tommy-regular-outline",
+  display: "swap",
+});
+
+export const madeTommyMedium = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY Medium_PERSONAL USE.otf",
+  variable: "--font-made-tommy-medium",
+  display: "swap",
+});
+
+export const madeTommyMediumOutline = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY Medium Outline_PERSONAL USE.otf",
+  variable: "--font-made-tommy-medium-outline",
+  display: "swap",
+});
+
+export const madeTommyBold = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY Bold_PERSONAL USE.otf",
+  variable: "--font-made-tommy-bold",
+  display: "swap",
+});
+
+export const madeTommyBoldOutline = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY Bold Outline_PERSONAL USE.otf",
+  variable: "--font-made-tommy-bold-outline",
+  display: "swap",
+});
+
+export const madeTommyExtraBold = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY ExtraBold_PERSONAL USE.otf",
+  variable: "--font-made-tommy-extrabold",
+  display: "swap",
+});
+
+export const madeTommyExtraBoldOutline = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY ExtraBold Outline_PERSONAL USE.otf",
+  variable: "--font-made-tommy-extrabold-outline",
+  display: "swap",
+});
+
+export const madeTommyBlack = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY Black_PERSONAL USE.otf",
+  variable: "--font-made-tommy-black",
+  display: "swap",
+});
+
+export const madeTommyBlackOutline = localFont({
+  src: "../public/fonts/made-tommy/MADE TOMMY Black Outline_PERSONAL USE.otf",
+  variable: "--font-made-tommy-black-outline",
+  display: "swap",
+});
+
+export const madeTommyFamily = localFont({
+  src: [
+    {
+      path: "../public/fonts/made-tommy/MADE TOMMY Thin_PERSONAL USE.otf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/made-tommy/MADE TOMMY Light_PERSONAL USE.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/made-tommy/MADE TOMMY Regular_PERSONAL USE.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/made-tommy/MADE TOMMY Medium_PERSONAL USE.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/made-tommy/MADE TOMMY Bold_PERSONAL USE.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/made-tommy/MADE TOMMY ExtraBold_PERSONAL USE.otf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/made-tommy/MADE TOMMY Black_PERSONAL USE.otf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-made-tommy",
+  display: "swap",
+});
