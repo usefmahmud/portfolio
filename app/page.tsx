64a8354a94b9924b655e4f6e037fac8f6b5ec752
usefmahmud <usef.mahmoud@outlook.com>
2025-07-18 15:57:56 +0300

    feat: set the initial data of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import { Me } from '@/components/misc/me';
+
 export default function Home() {
-  return <div className='text-center text-5xl'>Yousef Mahmoud</div>;
+  return (
+    <div className='px-20 py-35'>
+      <div className='flex justify-between'>
+        <div className='flex flex-col gap-6'>
+          <div className='flex flex-col gap-2 text-[50px] leading-[53px] font-bold'>
+            <h2>I'M A HUMAN</h2>
+            <h2>FRONTEND DEVELOPER</h2>
+          </div>
+          <p className='text-[40px] font-bold'>
+            CRAFTING INTUITIVE UIs & SLEEK CODE.
+          </p>
+        </div>
+
+        <div>
+          <div className='text-foreground h-40'>
+            <Me />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
